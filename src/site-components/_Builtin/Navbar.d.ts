8fc2afcef542e31302c416046d4dac2783393d28
RR-Gary-Stringham <gary@revrebel.io>
2025-12-03 03:38:08 +0000

    updates & fixes

--- src/site-components/_Builtin/Navbar.d.ts
+++ src/site-components/_Builtin/Navbar.d.ts
@@ -1,22 +1,21 @@
-import * as React from "react";
-import { EASING_FUNCTIONS } from "../utils";
-import { LinkProps, TagProps } from "./Basic";
+import * as React from 'react';
+import { EASING_FUNCTIONS } from '../utils';
+import { LinkProps, TagProps } from './Basic';
 declare const BREAKPOINTS: {
-  medium: number;
-  small: number;
-  tiny: number;
+    medium: number;
+    small: number;
+    tiny: number;
 };
 type NavbarConfig = {
-  animation: string;
-  collapse: keyof typeof BREAKPOINTS;
-  docHeight: boolean;
-  duration: number;
-  easing: keyof typeof EASING_FUNCTIONS;
-  easing2: keyof typeof EASING_FUNCTIONS;
-  noScroll: boolean;
+    animation: string;
+    collapse: keyof typeof BREAKPOINTS;
+    docHeight: boolean;
+    duration: number;
+    easing: keyof typeof EASING_FUNCTIONS;
+    easing2: keyof typeof EASING_FUNCTIONS;
+    noScroll: boolean;
 };
-export declare const NavbarContext: React.Context<
-  NavbarConfig & {
+export declare const NavbarContext: React.Context<NavbarConfig & {
     animDirect: -1 | 1;
     animOver: boolean;
     getBodyHeight: () => number | void;
@@ -27,93 +26,74 @@ export declare const NavbarContext: React.Context<
     toggleOpen: () => void;
     navbarMounted: boolean;
     setFocusedLink: React.Dispatch<React.SetStateAction<number>>;
-  }
->;
-type NavbarChildrenType =
-  | NavbarContainerProps
-  | NavbarBrandProps
-  | NavbarMenuProps
-  | NavbarButtonProps;
+}>;
+type NavbarChildrenType = NavbarContainerProps | NavbarBrandProps | NavbarMenuProps | NavbarButtonProps;
 type NavbarProps = {
-  tag: React.ElementType;
-  config: NavbarConfig;
-  className?: string;
-  children?:
-    | React.ReactElement<NavbarChildrenType>[]
-    | React.ReactElement<NavbarChildrenType>;
+    tag: React.ElementType;
+    config: NavbarConfig;
+    className?: string;
+    children?: React.ReactElement<NavbarChildrenType>[] | React.ReactElement<NavbarChildrenType>;
 };
-export declare const NavbarWrapper: React.ForwardRefExoticComponent<
-  NavbarProps & React.RefAttributes<unknown>
->;
+export declare const NavbarWrapper: React.ForwardRefExoticComponent<NavbarProps & React.RefAttributes<unknown>>;
 type NavbarContainerProps = TagProps & {
-  toggleOpen: () => void;
-  isOpen: boolean;
-  children: React.ReactNode;
-};
-export declare const NavbarContainer: React.ForwardRefExoticComponent<
-  import("./Basic").ElementProps<keyof HTMLElementTagNameMap> & {
-    tag?: React.ElementType;
-  } & {
-    children?: React.ReactNode | undefined;
-  } & {
     toggleOpen: () => void;
     isOpen: boolean;
     children: React.ReactNode;
-  } & React.RefAttributes<unknown>
->;
+};
+export declare const NavbarContainer: React.ForwardRefExoticComponent<import("./Basic").ElementProps<keyof HTMLElementTagNameMap> & {
+    tag?: React.ElementType;
+} & {
+    children?: React.ReactNode | undefined;
+} & {
+    toggleOpen: () => void;
+    isOpen: boolean;
+    children: React.ReactNode;
+} & React.RefAttributes<unknown>>;
 type NavbarBrandProps = LinkProps;
-export declare const NavbarBrand: React.ForwardRefExoticComponent<
-  import("./Basic").ElementProps<"a"> & {
+export declare const NavbarBrand: React.ForwardRefExoticComponent<import("./Basic").ElementProps<"a"> & {
     options?: {
-      href: string;
-      target?: "_self" | "_blank";
-      preload?: "none" | "prefetch" | "prerender";
+        href: string;
+        target?: "_self" | "_blank";
+        preload?: "none" | "prefetch" | "prerender";
     };
     className?: string;
     button?: boolean;
     block?: string;
-  } & {
+} & {
     children?: React.ReactNode | undefined;
-  } & React.RefAttributes<HTMLAnchorElement>
->;
+} & React.RefAttributes<HTMLAnchorElement>>;
 type NavbarMenuProps = React.PropsWithChildren<{
-  tag?: React.ElementType;
-  className?: string;
-  isOpen?: boolean;
-}>;
-export declare const NavbarMenu: React.ForwardRefExoticComponent<
-  {
     tag?: React.ElementType;
     className?: string;
     isOpen?: boolean;
-  } & {
+}>;
+export declare const NavbarMenu: React.ForwardRefExoticComponent<{
+    tag?: React.ElementType;
+    className?: string;
+    isOpen?: boolean;
+} & {
     children?: React.ReactNode | undefined;
-  } & React.RefAttributes<unknown>
->;
-export declare const NavbarLink: React.ForwardRefExoticComponent<
-  import("./Basic").ElementProps<"a"> & {
+} & React.RefAttributes<unknown>>;
+export declare const NavbarLink: React.ForwardRefExoticComponent<import("./Basic").ElementProps<"a"> & {
     options?: {
-      href: string;
-      target?: "_self" | "_blank";
-      preload?: "none" | "prefetch" | "prerender";
+        href: string;
+        target?: "_self" | "_blank";
+        preload?: "none" | "prefetch" | "prerender";
     };
     className?: string;
     button?: boolean;
     block?: string;
-  } & {
+} & {
     children?: React.ReactNode | undefined;
-  } & React.RefAttributes<HTMLAnchorElement>
->;
+} & React.RefAttributes<HTMLAnchorElement>>;
 type NavbarButtonProps = React.PropsWithChildren<{
-  tag?: React.ElementType;
-  className?: string;
-}>;
-export declare const NavbarButton: React.ForwardRefExoticComponent<
-  {
     tag?: React.ElementType;
     className?: string;
-  } & {
+}>;
+export declare const NavbarButton: React.ForwardRefExoticComponent<{
+    tag?: React.ElementType;
+    className?: string;
+} & {
     children?: React.ReactNode | undefined;
-  } & React.RefAttributes<unknown>
->;
+} & React.RefAttributes<unknown>>;
 export {};
--- src/site-components/_Builtin/YouTubeVideo.d.ts
+++ src/site-components/_Builtin/YouTubeVideo.d.ts
@@ -1,17 +1,15 @@
-import * as React from "react";
+import * as React from 'react';
 type YouTubeVideoProps = {
-  className?: string;
-  title: string;
-  videoId: string;
-  aspectRatio?: number;
-  startAt?: number;
-  showAllRelatedVideos?: boolean;
-  controls?: boolean;
-  autoplay?: boolean;
-  muted?: boolean;
-  privacyMode?: boolean;
+    className?: string;
+    title: string;
+    videoId: string;
+    aspectRatio?: number;
+    startAt?: number;
+    showAllRelatedVideos?: boolean;
+    controls?: boolean;
+    autoplay?: boolean;
+    muted?: boolean;
+    privacyMode?: boolean;
 };
-export declare const YouTubeVideo: React.ForwardRefExoticComponent<
-  YouTubeVideoProps & React.RefAttributes<HTMLDivElement>
->;
+export declare const YouTubeVideo: React.ForwardRefExoticComponent<YouTubeVideoProps & React.RefAttributes<HTMLDivElement>>;
 export {};
